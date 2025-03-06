9c8befec2bfd086cc04a109ea263592576be3320
psmohammedali <psmohammedali4@gmail.com>
2025-03-06 23:32:10 +0530

    input 2d array code

---
+++ 2_dimensional_array/input_2d_array.py
@@ -0,0 +1,24 @@
+# taking input from the user
+# 2
+# 1 2 3 4
+# 5 6 7 8
+
+rows = int(input())
+# for loop
+
+# new_2d_list = []
+# for row_n in range(rows):
+#     row_input = input().split(" ")
+#     integer_row = []
+#     for i in row_input:
+#         integer_row.append(int(i))
+#     new_2d_list.append(integer_row)
+#
+# print(new_2d_list)
+
+
+new_2d_list = [[int(char) for char in input().split()] for row_num in range(rows)]
+print(new_2d_list)
+
+
+
